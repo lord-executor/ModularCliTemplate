94c017c49bf60fea21c46d6cc09ccc84e1677131
Lukas Angerer <executor.dev@gmail.com>
2024-05-20 13:44:08 +0200

    Factored out Launcher class

---
+++ CliTemplate/Launcher.cs
@@ -0,0 +1,60 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
+using System.Reflection;
+
+using Microsoft.Extensions.Configuration;
+
+namespace CliTemplate;
+
+public class Launcher
+{
+    public HandlerFactory HandlerFactory { get; }
+
+    public Launcher()
+    {
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json")
+            .AddJsonFile("secrets.user.json", optional: true)
+            .AddJsonFile("secrets.json", optional: true)
+            .AddJsonFile($"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/{Assembly.GetEntryAssembly()?.GetName().Name ?? "cli"}.json", optional: true)
+            .Build();
+
+        HandlerFactory = new HandlerFactory(config);
+    }
+
+    public async Task<int> InvokeAsync(RootCommand rootCommand, string[] args)
+    {
+        var parser = new CommandLineBuilder(rootCommand)
+            // Basically the same as UseDefaults() but with a custom exception handler
+            .UseVersionOption()
+            .UseHelp()
+            .UseEnvironmentVariableDirective()
+            .UseParseDirective()
+            .UseSuggestDirective()
+            .RegisterWithDotnetSuggest()
+            .UseTypoCorrections()
+            .UseParseErrorReporting()
+            // Custom exception handler
+            .UseExceptionHandler(OnException)
+            .CancelOnProcessTermination()
+            .Build();
+
+        return await parser.InvokeAsync(args);
+    }
+
+    private static void OnException(Exception ex, InvocationContext ctx)
+    {
+        if (ex is OperationCanceledException)
+        {
+            ctx.Console.WriteLine("The operation was aborted");
+            ctx.ExitCode = ExitCodes.Aborted;
+            return;
+        }
+
+        ctx.Console.Error.WriteLine(ex.ToString());
+        ctx.ExitCode = ExitCodes.UnhandledException;
+    }
+}
--- CliTemplate/Program.cs
+++ CliTemplate/Program.cs
@@ -1,50 +1,13 @@
 using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.IO;
-using System.CommandLine.Parsing;
-using System.Reflection;
 
 using CliTemplate;
 using CliTemplate.Status;
 
-using Microsoft.Extensions.Configuration;
-
-var config = new ConfigurationBuilder()
-    .AddJsonFile("appSettings.json")
-    .AddJsonFile("secrets.user.json", optional: true)
-    .AddJsonFile("secrets.json", optional: true)
-    .AddJsonFile($"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/{Assembly.GetEntryAssembly()?.GetName().Name ?? "cli"}.json", optional: true)
-    .Build();
-
-var handlerFactory = new HandlerFactory(config);
+var launcher = new Launcher();
 
 var rootCommand = new RootCommand("TODO: Custom CLI tool for ???")
 {
-    new StatusCommand(handlerFactory),
+    new StatusCommand(launcher.HandlerFactory),
 };
 
-var parser = new CommandLineBuilder(rootCommand)
-    .UseVersionOption()
-    .UseHelp()
-    .UseEnvironmentVariableDirective()
-    .UseParseDirective()
-    .UseSuggestDirective()
-    .RegisterWithDotnetSuggest()
-    .UseTypoCorrections()
-    .UseParseErrorReporting()
-    .UseExceptionHandler((ex, ctx) =>
-    {
-        if (ex is OperationCanceledException)
-        {
-            ctx.Console.WriteLine("The operation was aborted");
-            ctx.ExitCode = ExitCodes.Aborted;
-            return;
-        }
-
-        ctx.Console.Error.WriteLine(ex.ToString());
-        ctx.ExitCode = ExitCodes.UnhandledException;
-    })
-    .CancelOnProcessTermination()
-    .Build();
-
-return await parser.InvokeAsync(args);
+return await launcher.InvokeAsync(rootCommand, args);
