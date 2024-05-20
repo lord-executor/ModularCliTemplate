5a89b6bb8c98c48250f9bc0920bb7043cc76d404
Lukas Angerer <executor.dev@gmail.com>
2024-05-20 13:08:34 +0200

    Working with ProcessStartInfo and Process with redirection

--- CliTemplate/Status/StatusHandler.cs
+++ CliTemplate/Status/StatusHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
+using System.Diagnostics;
 
 namespace CliTemplate.Status;
 
@@ -16,8 +17,41 @@ public class StatusHandler : ISimpleHandler<StatusArgs>
     public async Task<int> RunAsync(InvocationContext context, StatusArgs args, CancellationToken cancellationToken = default)
     {
         _console.WriteLine("Hello StatusHandler!");
-        await Task.Delay(5000, cancellationToken);
-        _console.WriteLine("Done");
+        _console.WriteLine($"Running in {Environment.CurrentDirectory}");
+
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "git",
+            WorkingDirectory = Environment.CurrentDirectory,
+            ArgumentList = { "log", "--color" },
+            UseShellExecute = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+        };
+
+        var p = new Process() { StartInfo = processStartInfo };
+        p.OutputDataReceived += (_, eventArgs) =>
+        {
+            if (eventArgs.Data != null)
+            {
+                _console.Out.WriteLine(eventArgs.Data!);
+            }
+        };
+        p.ErrorDataReceived += (_, eventArgs) =>
+        {
+            if (eventArgs.Data != null)
+            {
+                _console.Error.WriteLine(eventArgs.Data!);
+            }
+        };
+        p.Start();
+
+        p.BeginErrorReadLine();
+        p.BeginOutputReadLine();
+
+        await p.WaitForExitAsync(cancellationToken);
 
         return ExitCodes.Ok;
     }
